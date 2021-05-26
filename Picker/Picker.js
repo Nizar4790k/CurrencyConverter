818edaf91bdda6f0131b8ab08508e6c6aeef098d
Nizar Sanchez <nizar4790k@gmail.com>
2021-05-26 19:26:55 -0400

    component structure was created

---
+++ Picker/Picker.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const Picker = ()=>{
+
+    return(
+
+            <View>
+                <Text>Hello</Text>
+            </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default Picker;
